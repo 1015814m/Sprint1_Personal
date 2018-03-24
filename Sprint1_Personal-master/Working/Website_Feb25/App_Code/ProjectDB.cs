a30e1b12e95a2b699fac07898bdd362173694dbe
1015814m <1015814morrissey@gmail.com>
2018-03-24 14:37:09 -0400

    Add files via upload

---
+++ Sprint1_Personal-master/Working/Website_Feb25/App_Code/ProjectDB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+/*This class will contain most of the methods 
+ * for interacting with the database */
+namespace database
+{
+    public class ProjectDB
+    {
+        public static SqlConnection connectToDB()
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection
+                {
+                    ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["Sprint1Dev"].ConnectionString;
+                };
+                conn.Open();
+                return conn;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        
+    }
+
+    
+}
